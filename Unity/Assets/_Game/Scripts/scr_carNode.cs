6affb6485796548a70406afed23a612ff4ac0639
tobias02121999 <hedgefoggames@outlook.com>
2018-12-17 15:46:44 +0100

    Update 0.1
    - Core features added

---
+++ Unity/Assets/_Game/Scripts/scr_carNode.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_carNode : MonoBehaviour
+{
+    // Initialize the public variables
+    public scr_gameManager gameManager;
+    public int carNodeTeamID;
+    public float movementSpeed;
+    public float waypointDistanceThreshold;
+    public AudioSource audioSource;
+
+    // Initialize the private variables
+    int waypointCurrent;
+    bool audioIsPlaying;
+
+    // Run this code every frame
+    void Update()
+    {
+        // Check if the carNode is currently not at the correct waypoint (causing it to move to the correct one)
+        if (waypointCurrent != gameManager.teamScore[carNodeTeamID])
+        {
+            move(movementSpeed, waypointDistanceThreshold); // Move the carNode towards the next waypoint
+
+            if (!audioIsPlaying)
+            {
+                audioSource.Play();
+                audioIsPlaying = true;
+            }
+        }
+        else
+            audioIsPlaying = false;
+    }
+
+    // Move the carNode towards the next waypoint
+    void move(float speed, float distanceToReach)
+    {
+        var targetVector = gameManager.pathWaypoints[waypointCurrent + 1].position;
+        transform.position = Vector3.Lerp(transform.position, targetVector, speed);
+
+        var distance = Vector3.Distance(transform.position, targetVector);
+        if (distance <= distanceToReach)
+            waypointCurrent++;
+    }
+}
---
+++ Unity/Assets/_Game/Scripts/scr_gameManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_gameManager : MonoBehaviour
+{
+    // Initialize the public variables
+    public string[] teamColor;
+    public int[] teamScore;
+    public Transform[] pathWaypoints;
+
+    public GameObject carSpriteRed;
+    public GameObject carSpriteGreen;
+    public GameObject careSpriteBlue;
+
+    // Initialize the private variables
+    int teamPlaceFirstID;
+
+    // Run this code every frame
+    void Update()
+    {
+        // Find the top three highest-scoring teams
+        getTeamPlacing();
+        getTeamColor();
+    }
+
+    // Get the leading player
+    void getTeamPlacing()
+    {
+        var first = 0;
+
+        for (var i = 0; i < teamScore.Length; i++)
+        {
+            if (teamScore[i] > teamScore[first])
+                first = i;
+        }
+
+        teamPlaceFirstID = first;
+    }
+
+    // Get the color of the leading player
+    void getTeamColor()
+    {
+        switch (teamColor[teamPlaceFirstID])
+        {
+            case "RED":
+                carSpriteRed.SetActive(true);
+                carSpriteGreen.SetActive(false);
+                careSpriteBlue.SetActive(false);
+                break;
+
+            case "GREEN":
+                carSpriteRed.SetActive(false);
+                carSpriteGreen.SetActive(true);
+                careSpriteBlue.SetActive(false);
+                break;
+
+            case "BLUE":
+                carSpriteRed.SetActive(false);
+                carSpriteGreen.SetActive(false);
+                careSpriteBlue.SetActive(true);
+                break;
+        }
+    }
+}
